8ecf681262eec3b16601a36787bc31a6c2195817
legnaro72 <massimiliano.ferrando@gmail.com>
2026-05-11 19:22:19 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 // astro.config.mjs
-// IMPORTANTE: Modifica USERNAME e NOME-REPOSITORY con i tuoi valori GitHub
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://legnaro72.github.io',
   base: '/dediche-musicali',
-  integrations: [sitemap()],
   output: 'static',
 });
